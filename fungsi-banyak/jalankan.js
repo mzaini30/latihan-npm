cbbf7210b425bb4a49fcdb1ab63eb090970dcc3b
mzaini30 <muhzaini30@gmail.com>
2021-02-13 12:20:05 +0800

    upload

---
+++ fungsi-banyak/jalankan.js
@@ -0,0 +1,8 @@
+// import
+const teksnya = require('./teks')
+// jalankan
+teksnya.lapar()
+teksnya.hello()
+teksnya.hello()
+teksnya.hello()
+teksnya.balik()
---
+++ fungsi-banyak/teks.js
@@ -0,0 +1,6 @@
+// list semua fungsi
+const hello = () => console.log('Hello world...')
+const lapar = () => console.log('Aku lapar nih...')
+const balik = () => console.log('Udahan ya...')
+// export
+module.exports = {hello, lapar, balik}
